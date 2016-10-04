4161676706231dcf303517d781d5ab3ee03f0e1c
Jason Stanton <Jason Stanton>
2016-10-04 14:07:54 +1100

    Added Easy AI difficulty and adjusted unit test suitably.

--- battle-shipsTests/src/GameControllerTests.cs
+++ battle-shipsTests/src/GameControllerTests.cs
@@ -15,7 +15,6 @@ namespace Tests
          * This Test method runs assertions on the difficulty of the AI player in the game.
          * This testing is quite complicated as there is no methods to pull an AI players difficulty.
          * Instead we test that on a given difficulty, the appropriate AI player is made to match that difficulty.
-         * Currently there is no Easy AIPlayer, so this cannot adequately be tested, and must become an extension task.
          */
         [TestMethod()]
         public void SetDifficultyTest()
@@ -31,17 +30,17 @@ namespace Tests
                 switch (difficulty) {
                     case AIOption.Medium:
                         _ai = new AIMediumPlayer(_theGame);
-                    break;
+                        break;
                     case AIOption.Hard:
                         _ai = new AIHardPlayer(_theGame);
-                    break;
-                    /*
-                     * default:
-                     *  _ai = new AIEasyPlayer(_theGame);
-                     *  
-                     *  Above lines need to be uncommented when EasyAIPlayer
-                     *  class created
-                     */
+                        break;
+                    case AIOption.Easy:
+                        _ai = new AIEasyPlayer(_theGame);
+                        break;
+                    default:
+                        _ai = new AIEasyPlayer(_theGame);
+                        break;
+                     
 
                 }
             });
@@ -50,14 +49,7 @@ namespace Tests
             difficulty = AIOption.Easy;
             GameController.SetDifficulty(difficulty);
             CreateAIPlayer.Invoke();
-            System.Console.Write(Environment.CurrentDirectory);
-            if (!File.Exists("../../../battle-ships/src/Model/AIEasyPlayer.cs"))   // test if easy ai player class file exists
-                Assert.Fail(); // if it doesn't fail the test.
-
-            /* Two lines above need to be removed and updated
-             * with following line when easy class has been created.
-             * Assert.IsInstanceOfType(_ai, typeof(AIEasyPlayer));
-             */
+            Assert.IsInstanceOfType(_ai, typeof(AIEasyPlayer));
 
             // Set difficulty to medium and assert that the correct AIPlayer is made.
             difficulty = AIOption.Medium;
