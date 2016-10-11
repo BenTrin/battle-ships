0a8a9cfd20fa8e940ebdf66685b975b4bb0a62f1
7682840 <7682840@student.swin.edu.au>
2016-10-12 10:22:37 +1100

    Added code to unit test

--- battle-shipsTests/src/AttackResultTests.cs
+++ battle-shipsTests/src/AttackResultTests.cs
@@ -19,10 +19,40 @@ namespace Tests
         [TestMethod()]
         public void HitResultTest()
         {
-            Ship Tug = new Ship(ShipName.Tug);
+            //Create New game
+            BattleShipsGame _game = new BattleShipsGame();
 
-            
-            
+            //Create new player
+            Player _player = new Player(_game);
+
+            //Create new AI
+            AIPlayer _ai = new AIHardPlayer(_game);
+            AIOption _aiSetting;
+            _aiSetting = AIOption.Easy;
+            GameController.SetDifficulty(_aiSetting);
+            GameController.CreateAI(_aiSetting, ref _ai, _game);
+
+            _player.RandomizeDeployment();
+            _ai.RandomizeDeployment();
+
+            //Copy the tug ship in player to a variable
+            //Need to do this in order to access the row and column
+            Ship _aiTug = _ai.Ship(ShipName.Tug);
+
+            //Get coordinates of the tug ship
+            int _tugRow = _aiTug.Row;
+            int _tugColumn = _aiTug.Column;
+
+            //Expect result to be a hit
+            ResultOfAttack _hitResult = ResultOfAttack.Hit;
+            AttackResult _expectedResult = new AttackResult(_hitResult, "Hit", _tugRow, _tugColumn);
+
+            ResultOfAttack _missResult = ResultOfAttack.Miss;
+            AttackResult _actualResult = new AttackResult(_missResult, "Miss", 0, 0);
+            //AttackResult _actualResult = default(AttackResult);
+            _actualResult = _game.Shoot(0,0);
+
+            Assert.AreEqual(_actualResult.Value, _expectedResult.Value);
         }
     }
 }
